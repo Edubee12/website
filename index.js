9868a77d666ff9bc8d8e04bdf9103fd0b5b789b2
Edubee12 <kageka2007@gmail.com>
2023-05-19 11:46:07 +0300

    changed port number

--- index.js
+++ index.js
@@ -522,12 +522,7 @@ app.get('/admin', ensureAuthenticated, function(req, res) {
   res.render('admin');
 });
 
-
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000
-}
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server started on port 3000")
-})
+  console.log(`Server started on port ${port}`);
+});
